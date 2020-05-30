5eb70cd50ed50b715de5d72a98041eaac1769666
Elaine <bing_elaine@sina.com>
2020-05-30 11:30:51 +0800

    git add .

---
+++ src/App1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>app</h1>
+    <P class="p"></P>
+    <van-button type="info">信息按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.p{
+  width: 100px;
+  height: 100px;
+  background-color: blueviolet;
+}
+</style>
